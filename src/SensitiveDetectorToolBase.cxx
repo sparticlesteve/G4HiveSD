6b23cf978307ad1fa83bd87ec75afef5bba8d2ae
Steve Farrell <sfarrell@cern.ch>
2015-04-09 08:41:10 +0200

    implemented initializeSD

--- src/SensitiveDetectorToolBase.cxx
+++ src/SensitiveDetectorToolBase.cxx
@@ -17,5 +17,19 @@ SensitiveDetectorToolBase::SensitiveDetectorToolBase(const std::string& type,
 //-----------------------------------------------------------------------------
 StatusCode SensitiveDetectorToolBase::initializeSD()
 {
+  // Get current thread-ID. I believe it's ok to use std::thread::id even
+  // though we're working with TBB.
+  const auto id = std::this_thread::get_id();
+
+  // Make sure there is not already an SD assigned to this thread
+  if(m_sdThreadMap.find(id) != m_sdThreadMap.end()){
+    ATH_MSG_ERROR("Already registered an SD for thread " << id);
+    return StatusCode::FAILURE;
+  }
+
+  // Create a new SD and add it to the map
+  G4VSensitiveDetector* sd = makeSD();
+  m_sdThreadMap.insert( std::make_pair(id, sd) );
+
   return StatusCode::SUCCESS;
 }
